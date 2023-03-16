fixed units in plots

quantifiles/plot/colorplot.py
@@ -4,6 +4,9 @@ import pyqtgraph
 import numpy as np
 import xarray as xr
 
+from quantifiles import units
+from quantifiles.plot.utils import set_label
+
 COLORMAP = "viridis"
 
 
@@ -24,12 +27,15 @@ class ColorPlot(QtWidgets.QWidget):
         self.img.setColorMap(pyqtgraph.colormap.get(COLORMAP))
         self.colorbar = pyqtgraph.ColorBarItem()
 
+        x_unit, x_scaling = units.get_si_unit_and_scaling(dataset[x].attrs["units"])
+        y_unit, y_scaling = units.get_si_unit_and_scaling(dataset[y].attrs["units"])
+
         is_uniformly_spaced = dataset and dataset.attrs.get(
             "grid_2d_uniformly_spaced", dataset.attrs.get("2D-grid", False)
         )
         if is_uniformly_spaced:
-            x_data = dataset[x].values[: dataset.attrs["xlen"]]
-            y_data = dataset[y].values[:: dataset.attrs["xlen"]]
+            x_data = x_scaling * dataset[x].values[: dataset.attrs["xlen"]]
+            y_data = y_scaling * dataset[y].values[:: dataset.attrs["xlen"]]
             z_data = np.reshape(
                 dataset[z].values, (len(x_data), len(y_data)), order="F"
             )
@@ -40,16 +46,11 @@ class ColorPlot(QtWidgets.QWidget):
             )
         self.setLayout(QtWidgets.QVBoxLayout())
         self.layout().addWidget(self.plot)
-
-        self.plot.setLabel(
-            "left",
-            self.dataset[y].long_name,
-            units=self.dataset[y].attrs["units"],
+        set_label(
+            self.plot, "bottom", dataset[x].long_name, x_unit, dataset[x].attrs["units"]
         )
-        self.plot.setLabel(
-            "bottom",
-            self.dataset[x].long_name,
-            units=self.dataset[x].attrs["units"],
+        set_label(
+            self.plot, "left", dataset[y].long_name, y_unit, dataset[y].attrs["units"]
         )
 
     def set_image(self, x_data: np.ndarray, y_data: np.ndarray, z_data: np.ndarray):