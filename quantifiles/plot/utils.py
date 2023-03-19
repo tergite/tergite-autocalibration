001b55671af44653af7ac1173c9245baf0a38f35
Damien Crielaard <damiencrielaard@gmail.com>
2023-03-19 16:49:34 +0100

    cleanup

--- quantifiles/plot/utils.py
+++ quantifiles/plot/utils.py
@@ -3,15 +3,33 @@ from __future__ import annotations
 from pyqtgraph.widgets import PlotWidget
 
 
-def set_label(plot: PlotWidget, axis: str, name: str, unit: str | None, default_unit: str):
-    if unit is not None:
-        plot.setLabel(
-            axis,
-            name,
-            units=unit,
-        )
-    else:
-        plot.setLabel(
-            axis,
-            f"{name} ({default_unit})",
-        )
+def set_label(
+    plot: PlotWidget, axis: str, name: str, unit: str | None, default_unit: str
+):
+    """
+    Set the label of the specified axis on the given plot widget.
+
+    Parameters
+    ----------
+    plot : PlotWidget
+        The plot widget to set the label on.
+    axis : str
+        The axis to set the label for ('left', 'bottom', 'right', or 'top').
+    name : str
+        The name of the label to set.
+    unit : str, optional
+        The units of the label (e.g. 'm', 's', 'kg'). If `None`, the `default_unit` will be used and pyqtgraph will not
+        rescale the axis. We assume that this is always an SI unit.
+    default_unit : str
+        The default units to use if `unit` is `None`.
+
+    Returns
+    -------
+    None
+    """
+    label = f"{name} ({default_unit})" if unit is None else name
+    plot.setLabel(
+        axis,
+        label,
+        units=unit,
+    )
