1798f4a1d4a20a06460b7b67d687d97dc22696f3
Damien Crielaard <damiencrielaard@gmail.com>
2023-03-19 20:11:33 +0100

    remove nonsense test

--- tests/test_units.py
+++ tests/test_units.py
@@ -15,9 +15,5 @@ def test_format_value_and_unit_known_unit():
     assert format_value_and_unit(1, "uA") == "1.0000e-06A"
 
 
-def test_format_value_and_unit_unknown_unit():
-    assert format_value_and_unit(1e-6, "foo") == "1.0000e-06"
-
-
 def test_format_value_and_unit_nan():
     assert format_value_and_unit(np.nan, "uA") == "0.0000e+00A"
