8019d06741fdc1d3fce13f34108f2c5e732d4c0a
Damien Crielaard <damiencrielaard@gmail.com>
2023-03-19 20:07:02 +0100

    fix tests

--- tests/test_units.py
+++ tests/test_units.py
@@ -12,7 +12,7 @@ def test_get_si_unit_and_scaling_unknown_unit():
 
 
 def test_format_value_and_unit_known_unit():
-    assert format_value_and_unit(1e-6, "uA") == "1.0000e-06A"
+    assert format_value_and_unit(1, "uA") == "1.0000e-06A"
 
 
 def test_format_value_and_unit_unknown_unit():
