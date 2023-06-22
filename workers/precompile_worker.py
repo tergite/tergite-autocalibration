1a8659cc24b836dc2df143eb232869f7892607c3
lefteris <elemos@chalmers.se>
2023-06-23 01:16:04 +0300

    feat: initial workers directory

---
+++ workers/precompile_worker.py
@@ -0,0 +1,6 @@
+'''Given the requested node
+fetch and compile the appropriate schedule'''
+import numpy as np
+
+def precompile(node:str, qubits: list[str], setpoints: np.ndarray):
+    print('precompiling!!!')
