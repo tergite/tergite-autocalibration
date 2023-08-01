7bfe8e1b9ed753f2035af3ebdef5dbf423fd7f4f
Andreas Lund <lundand@qtl-lundand.mc2.chalmers.se>
2023-08-01 11:42:36 +0200

    fix: Added a cluster reset when main_worker is run. Also added the T1 result as a qoi in the config file.

--- workers/main_worker.py
+++ workers/main_worker.py
@@ -10,6 +10,29 @@ redis_connection = redis.Redis(decode_responses=False)
 redis_connection.flushdb()
 queue = Queue('calibration_supervisor', connection=redis_connection)
 
+# Resets the clusters:
+from datetime import datetime
+from qblox_instruments import Cluster
+from qcodes.instrument.base import Instrument
+import threading
+
+def preset_cluster(name, IP):
+    cluster = Cluster(name,IP)
+    cluster.reset()
+    pass
+
+t0 = datetime.now()
+print('Reseting Clusters')
+thread_1 = threading.Thread(target=preset_cluster, args=('ClusterA', '192.0.2.72'))
+thread_1.start()
+thread_2 = threading.Thread(target=preset_cluster, args=('ClusterB', '192.0.2.141'))
+thread_2.start()
+thread_1.join()
+thread_2.join()
+t1 = datetime.now()
+print(f'Reseting time: { t1-t0 = }')
+Instrument.close_all()
+
 # Start fresh:
 queue.empty()
 workers = Worker.all(redis_connection)
