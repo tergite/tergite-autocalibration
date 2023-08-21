eaf666fccf9f69ae51d091e19dcb99ede6d9e339
lefteris <elemos@chalmers.se>
2023-08-21 23:17:07 +0300

    feat: motzoi schedule

--- calibration_schedules/motzoi_paramerter.py
+++ calibration_schedules/motzoi_paramerter.py
@@ -2,50 +2,20 @@ from quantify_scheduler.resources import ClockResource
 from quantify_scheduler import Schedule
 from quantify_scheduler.operations.pulse_library import DRAGPulse
 from quantify_scheduler.operations.gate_library import Measure, Reset
-from measurements_base import Measurement_base
+from calibration_schedules.measurement_base import Measurement
 
-class Motzoi_parameter(Measurement_base):
+class Motzoi_parameter(Measurement):
 
-    def __init__(self,transmons,connections):
-        super().__init__(transmons,connections)
-        self.experiment_parameters = ['mw_motzoi_BATCHED', 'X_repetition'] # The order maters
-        self.parameter_order = ['mw_motzoi_BATCHED', 'X_repetition'] # The order maters
-        self.gettable_batched = True
+    def __init__(self,transmons):
+        super().__init__(transmons)
+        self.transmons = transmons
         self.static_kwargs = {
             'qubits': self.qubits,
-            'mw_frequencies': self._get_attributes('freq_01'),
-            'mw_amplitudes': self._get_attributes('mw_amp180'),
-            'mw_clocks': self._get_attributes('mw_01_clock'),
-            'mw_pulse_ports': self._get_attributes('mw_port'),
-            'mw_pulse_durations': self._get_attributes('mw_pulse_duration'),
+            'mw_frequencies': self.attributes_dictionary('f01'),
+            'mw_amplitudes': self.attributes_dictionary('mw_amp180'),
+            'mw_pulse_durations': self.attributes_dictionary('duration'),
+            'mw_pulse_ports': self.attributes_dictionary('microwave'),
         }
-
-    def settables_dictionary(self):
-        parameters = self.experiment_parameters
-        manual_parameter = 'mw_motzoi_BATCHED'
-        assert( manual_parameter in self.experiment_parameters )
-        mp_data = {
-            manual_parameter : {
-                'name': manual_parameter,
-                'initial_value': 1e-4,
-                'unit': 'V',
-                'batched': True
-            }
-        }
-        manual_parameter = 'X_repetition'
-        assert( manual_parameter in self.experiment_parameters )
-        mp_data.update( {
-            manual_parameter: {
-                'name': manual_parameter,
-                'initial_value': 1,
-                'unit': '-',
-                'batched': False
-            }
-        })
-        return self._settables_dictionary(parameters, isBatched=self.gettable_batched, mp_data=mp_data)
-
-    def setpoints_array(self):
-        return self._setpoints_Nd_array()
 
     def schedule_function(
             self,
@@ -53,43 +23,34 @@ class Motzoi_parameter(Measurement_base):
             mw_frequencies: dict[str,float],
             mw_amplitudes: dict[str,float],
             mw_pulse_ports: dict[str,str],
-            mw_clocks: dict[str,str],
             mw_pulse_durations: dict[str,float],
+            mw_motzois: dict[str,np.ndarray],
+            X_repetitions: dict[str,np.ndarray],
             repetitions: int = 1024,
-            **mw_motzois,
         ) -> Schedule:
         schedule = Schedule("mltplx_motzoi",repetitions)
 
-        for mw_f_key, mw_f_val in mw_frequencies.items():
-            this_qubit = [q for q in qubits if q in mw_f_key][0]
+        for this_qubit, mw_f_val in mw_frequencies.items():
             schedule.add_resource(
-                ClockResource(name=mw_clocks[this_qubit], freq=mw_f_val)
+                ClockResource( name=f'{this_qubit}.01', freq=mw_f_val)
             )
-        values = {qubit:{} for qubit in qubits}
-        for motzoi_key, motzoi_val in mw_motzois.items():
-            this_qubit = [q for q in qubits if q in motzoi_key][0]
-            if 'X_repetition' in motzoi_key:
-               values[this_qubit].update({'X_repetition':motzoi_val})
-            if 'mw_motzoi' in motzoi_key:
-               values[this_qubit].update({'mw_motzoi':motzoi_val})
 
         #This is the common reference operation so the qubits can be operated in parallel
         root_relaxation = schedule.add(Reset(*qubits), label="Reset")
 
-        for acq_cha, (values_key, values_val) in enumerate(values.items()):
-            this_qubit = [q for q in qubits if q in values_key][0]
+        # The outer loop, iterates over all qubits
+        for acq_cha, (this_qubit, X_values) in enumerate(X_repetitions.items()):
 
-            X_repetitions = values_val['X_repetition']
-            X_repetitions = int(X_repetitions)
-            motzoi_parameter_values = values_val['mw_motzoi']
+            motzoi_parameter_values = mw_motzois[this_qubit]
+            number_of_motzois = len(motzoi_parameter_values)
 
             # The second for loop iterates over all frequency values in the frequency batch:
-            relaxation = schedule.add(
-                Reset(*qubits), label=f'Reset_{acq_cha}', ref_op=root_relaxation, ref_pt_new='end'
+            schedule.add(
+                Reset(*qubits), ref_op=root_relaxation, ref_pt_new='end'
             ) #To enforce parallelism we refer to the root relaxation
 
-            for acq_index, mw_motzoi in enumerate(motzoi_parameter_values):
-                for x_index in range(X_repetitions):
+            for motzoi_index, mw_motzoi in enumerate(motzoi_parameter_values):
+                for x_index in range(X_values):
                     schedule.add(
                         DRAGPulse(
                             duration=mw_pulse_durations[this_qubit],
