0c823ff0ba308c7ac5632f6c2c71f3f3ab8d570d
Eleftherios Moschandreou <elemos@chalmers.se>
2023-08-24 17:48:43 +0200

    feat: motzoi analysis cleanup

--- analysis/tac_quantify_analysis.py
+++ analysis/tac_quantify_analysis.py
@@ -83,51 +83,6 @@ class Multiplexed_Analysis(BaseAnalysis):
             this_axis.set(title=None)
             this_axis.legend(handles=handles)
 
-
-# class Multiplexed_Motzoi_Analysis(BaseAnalysis):
-#     def __init__(self, result_dataset: xr.Dataset, node: str):
-#         super().__init__(result_dataset)
-#         for indx, var in enumerate(result_dataset.data_vars):
-#             this_qubit = result_dataset[var].attrs['qubit']
-#             ds = result_dataset[var].to_dataset()
-#             ds.attrs['qubit'] = this_qubit
-#             motzoi_key = 'mw_motzois'+this_qubit
-#
-#             motzois = ds[motzoi_key].size
-#             sums = []
-#             for this_motzoi_index in range(motzois):
-#                 this_sum = sum(np.abs(ds[f'y{this_qubit}'][this_motzoi_index].values))
-#                 sums.append(this_sum)
-#
-#             index_of_min = np.argmin(np.array(sums))
-#             # breakpoint()
-#             optimal_motzoi = float(ds[motzoi_key][index_of_min].values)
-#
-#             this_axis = self.axs[indx//self.column_grid, indx%self.column_grid]
-#
-#             # ds.y0.plot(ax=this_axis)
-#             # this_axis.axvline(optimal_motzoi, c='red', lw=4)
-#             self.qoi = optimal_motzoi
-#             self.update_redis_trusted_values(node, this_qubit, 'mw_motzoi')
-#             # this_axis.set_title(f'{node_name} for {this_qubit}')
-#
-#             # this_axis = self.axs[indx//self.column_grid, indx%self.column_grid]
-#             # # this_axis.set_title(f'{node_name} for {this_qubit}')
-#             # node_analysis = MotzoiAnalysis(ds)
-#             # motzoi_parameter = node_analysis.run_fitting()
-#             #
-#             # self.qoi = motzoi_parameter
-#             #
-#             # node_analysis.plotter(this_axis)
-#             #
-#             # self.update_redis_trusted_values(node, this_qubit,'mw_motzoi')
-#             #
-#             # handles, labels = this_axis.get_legend_handles_labels()
-#             # patch = mpatches.Patch(color='red', label=f'{this_qubit}')
-#             # handles.append(patch)
-#             # this_axis.set(title=None)
-#             # this_axis.legend(handles=handles)
-
 #class Multiplexed_T1_Analysis(BaseAnalysis):
 #    def __init__(self, result_dataset: xr.Dataset, node: str):
 #        super().__init__(result_dataset)
