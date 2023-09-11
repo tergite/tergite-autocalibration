cd3548b36ebcf7b84a3e9e683ee71b8d316250b2
Eleftherios Moschandreou <elemos@chalmers.se>
2023-09-11 10:01:39 +0200

    feat: update QPU visualization

--- utilities/QPU_connections_visualization.py
+++ utilities/QPU_connections_visualization.py
@@ -14,20 +14,36 @@ class QPU_element:
         self.qubit_freq = VNA_f01_frequencies[self.label]
 
 QPU = [
-        QPU_element('q15', 'E9', 1, (4,2)),
-        QPU_element('q16', 'E9', 2, (0,1)),
+        QPU_element('q11', 'D6', 1, (0,2)),
+        QPU_element('q12', 'A1', 2, (1,2)),
+        QPU_element('q13', 'A5', 3, (2,2)),
+        QPU_element('q14', 'A9', 4, (3,2)),
+        QPU_element('q15', 'D4', 5, (4,2)),
+        QPU_element('q16', 'A3', 1, (0,1)),
+        QPU_element('q17', 'D2', 2, (1,1)),
+        QPU_element('q18', 'A2', 3, (2,1)),
+        QPU_element('q19', 'D3', 4, (3,1)),
+        QPU_element('q20', 'A8', 5, (4,1)),
+        QPU_element('q21', 'D5', 6, (0,0)),
+        QPU_element('q22', 'A4', 7, (1,0)),
+        QPU_element('q23', 'A6', 8, (2,0)),
+        QPU_element('q24', 'D7', 9, (3,0)),
+        QPU_element('q25', 'A7', 10,(4,0)),
       ]
 
-fig, ax = plt.subplots(1,1, figsize=(10.5,8))
+fig, ax = plt.subplots(1,1, figsize=(10,6))
 
 for element in QPU:
-    ax.add_patch(Rectangle(element.grid_coords,1,1,color='yellow', alpha=0.5))
+    x_coord, y_coord = element.grid_coords
+    x_coord *= 1.6
+    y_coord *= 1.6
+    ax.add_patch(Rectangle((x_coord,y_coord),1,1.2,color='dodgerblue', alpha=0.5))
     ann_text = f'{element.label}\nmodule{element.module}\n{element.XY_line}\n{element.res_freq:.3e}\n{element.qubit_freq:.3e}'
-    ann_style = {'fontsize': 18, 'fontweight': 'roman'}
-    ax.annotate(ann_text, element.grid_coords, **ann_style)
+    ann_style = {'fontsize': 14, 'fontweight': 'roman'}
+    ax.annotate(ann_text, (x_coord,y_coord), **ann_style)
 
-ax.set_xlim(left = 0, right = 5)
-ax.set_ylim(bottom = 0, top = 5)
+ax.set_xlim(left = 0, right = 5*1.6 - 0.6)
+ax.set_ylim(bottom = 0, top = 6)
 
 plt.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
 
