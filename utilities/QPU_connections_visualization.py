d666c289d513725a71cc46a0e9524c0a0e20e530
Eleftherios Moschandreou <elemos@chalmers.se>
2023-09-14 18:14:54 +0200

    feat: tools to detect collisions of frequencies

--- utilities/QPU_connections_visualization.py
+++ utilities/QPU_connections_visualization.py
@@ -1,7 +1,8 @@
-from config_files.VNA_values import VNA_f01_frequencies, VNA_resonator_frequencies
+from config_files.VNA_values import VNA_f01_frequencies, VNA_resonator_frequencies, VNA_f12_frequencies
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from dataclasses import dataclass
+import numpy as np
 
 @dataclass
 class QPU_element:
@@ -14,11 +15,11 @@ class QPU_element:
         self.qubit_freq = VNA_f01_frequencies[self.label]
 
 QPU = [
-        QPU_element('q11', 'D6', 1, (0,2)),
-        QPU_element('q12', 'A1', 2, (1,2)),
-        QPU_element('q13', 'A5', 3, (2,2)),
-        QPU_element('q14', 'A9', 4, (3,2)),
-        QPU_element('q15', 'D4', 5, (4,2)),
+        # QPU_element('q11', 'D6', 1, (0,2)),
+        # QPU_element('q12', 'A1', 2, (1,2)),
+        # QPU_element('q13', 'A5', 3, (2,2)),
+        # QPU_element('q14', 'A9', 4, (3,2)),
+        # QPU_element('q15', 'D4', 5, (4,2)),
         QPU_element('q16', 'A3', 1, (0,1)),
         QPU_element('q17', 'D2', 2, (1,1)),
         QPU_element('q18', 'A2', 3, (2,1)),
@@ -31,20 +32,60 @@ QPU = [
         QPU_element('q25', 'A7', 10,(4,0)),
       ]
 
-fig, ax = plt.subplots(1,1, figsize=(10,6))
+# fig, ax = plt.subplots(1,1, figsize=(10,6))
+# for element in QPU:
+#     x_coord, y_coord = element.grid_coords
+#     x_coord *= 1.6
+#     y_coord *= 1.6
+#     ax.add_patch(Rectangle((x_coord,y_coord),1,1.2,color='dodgerblue', alpha=0.5))
+#     ann_text = f'{element.label}\nmodule{element.module}\n{element.XY_line}\n{element.res_freq:.3e}\n{element.qubit_freq:.3e}'
+#     ann_style = {'fontsize': 14, 'fontweight': 'roman'}
+#     ax.annotate(ann_text, (x_coord,y_coord), **ann_style)
+# ax.set_xlim(left = 0, right = 5*1.6 - 0.6)
+# ax.set_ylim(bottom = 0, top = 6)
+# plt.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+# plt.show()
 
-for element in QPU:
-    x_coord, y_coord = element.grid_coords
-    x_coord *= 1.6
-    y_coord *= 1.6
-    ax.add_patch(Rectangle((x_coord,y_coord),1,1.2,color='dodgerblue', alpha=0.5))
-    ann_text = f'{element.label}\nmodule{element.module}\n{element.XY_line}\n{element.res_freq:.3e}\n{element.qubit_freq:.3e}'
-    ann_style = {'fontsize': 14, 'fontweight': 'roman'}
-    ax.annotate(ann_text, (x_coord,y_coord), **ann_style)
 
-ax.set_xlim(left = 0, right = 5*1.6 - 0.6)
-ax.set_ylim(bottom = 0, top = 6)
+# fig, ax = plt.subplots(1,1, figsize=(10,4))
+# for qubit, f01 in VNA_f01_frequencies.items():
+#     f12 = VNA_f12_frequencies[qubit]
+#     ax.axvline(x=f01)
+#     ax.axvline(x=f12)
+# plt.show()
 
-plt.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+def distance(element_1, element_2):
+     x_distance = np.abs(element_1.grid_coords[0] - element_2.grid_coords[0])
+     y_distance = np.abs(element_1.grid_coords[1] - element_2.grid_coords[1])
+     return x_distance + y_distance
 
-plt.show()
+
+
+
+
+LO_group = 3.644e9
+def hits_neighbors(qubit:str, lo_freq:float):
+    for q in QPU:
+        if q.label == qubit:
+            element = q
+    print(f'{ element = }')
+
+    # Code by Stefan Hill:
+    neighbour_qubits = list(filter(lambda element_: distance(element, element_)==1, QPU))
+
+    print(f'{ neighbour_qubits = }')
+    f01 = VNA_f01_frequencies[qubit]
+    f12 = VNA_f12_frequencies[qubit]
+    i_freq = f01 - lo_freq
+    mirror = lo_freq - i_freq
+    h_harm_2 = lo_freq - 2*i_freq
+    h_harm_3 = lo_freq - 3*i_freq
+    l_harm_2 = lo_freq + 2*i_freq
+    l_harm_3 = lo_freq + 3*i_freq
+    harmonics = [mirror, h_harm_2, h_harm_3, l_harm_2, l_harm_3]
+    for harmonic in harmonics:
+        if np.abs(harmonic-f12) < 5e6:
+            print(f'harmonic {harmonic} hits f12')
+
+
+hits_neighbors('q25', LO_group)
