63921c19aaad9fc1608eea489cf8ee061cb07c97
Eleftherios Moschandreou <elemos@chalmers.se>
2023-11-15 14:01:40 +0100

    feat: selecting optimal scec amplitude

--- analysis/qubit_spectroscopy_multidim.py
+++ analysis/qubit_spectroscopy_multidim.py
@@ -132,6 +132,7 @@ class QubitSpectroscopyMultidim():
         #ax.plot( self.fit_freqs, self.fit_y,'r-',lw=3.0)
         #print(f'{ self.dataset[self.data_var].shape = }')
         self.dataset[self.data_var].plot(ax=ax, x=self.frequency_coords)
+        ax.scatter(self.qubit_freq, self.qubit_ampl, s=52, c='red')
         # for i,a in enumerate(self.amplitudes):
         #     if a==self.qubit_ampl:
         #         label=f'Best amplitude:{self.qubit_ampl:.3E}'
--- open_ds.py
+++ open_ds.py
@@ -7,26 +7,28 @@ from analysis.coupler_spectroscopy_analysis import CouplerSpectroscopyAnalysis
 from numpy.polynomial.polynomial import Polynomial
 from nodes.node import NodeFactory
 from workers.post_processing_worker import post_process
+from utilities.user_input import qubits
 
 
-ds = xr.open_dataset('coupler20231113/dataset.hdf5')
+ds = xr.open_dataset('multidim20231115/20231115-132744-958-b74520-qubit_01_spectroscopy_multidim/dataset.hdf5')
 # print(f'{ ds.yq21.attrs = }')
 ds = ds.isel(ReIm=0) + 1j * ds.isel(ReIm=1)
 
 #---
-for data_var in ds.data_vars:
-    ds[data_var].attrs = {'qubit': data_var[1:4]}
+for d_var in ds.data_vars:
+    ds[d_var].attrs = {'qubit': d_var[1:4]}
 
 #---
-fig, ax = plt.subplots()
-#---
-d16 = ds.yq16.to_dataset(promote_attrs = True)
-d16.yq16.values = np.abs(ds.yq16.values)
+for qubit in qubits:
+    fig, ax = plt.subplots()
+    arr = 'y' + qubit
+    dataset = ds[arr].to_dataset(promote_attrs = True)
+    dataset[arr].values = np.abs(ds[arr].values)
 # print(f'{ d16 = }')
-analysis = QubitSpectroscopyMultidim(d16)
-analysis.run_fitting()
-analysis.plotter(ax)
-plt.show()
+    analysis = QubitSpectroscopyMultidim(dataset)
+    analysis.run_fitting()
+    analysis.plotter(ax)
+    plt.show()
 
 # data_var = list(ds.data_vars.keys())[0]
 
