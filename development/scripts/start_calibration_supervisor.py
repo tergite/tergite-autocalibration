7473f8c1dabe963ce08d10ddeee227a527424905
Stefan Hill <stefanhi@chalmers.se>
2024-06-24 13:40:59 +0200

    Moved files to correct locations

--- development/scripts/start_calibration_supervisor.py
+++ development/scripts/start_calibration_supervisor.py
@@ -2,11 +2,10 @@ from ipaddress import IPv4Address
 
 from tergite_acl.config.settings import CLUSTER_IP
 from tergite_acl.scripts.calibration_supervisor import CalibrationSupervisor
-
+from tergite_acl.utils.enums import ClusterMode
 
 cluster_mode: 'ClusterMode' = ClusterMode.real
 parsed_cluster_ip: 'IPv4Address' = CLUSTER_IP
-
 supervisor = CalibrationSupervisor(cluster_mode=cluster_mode,
                                     cluster_ip=parsed_cluster_ip)
 supervisor.calibrate_system()
