dce4cdfa9d8aff34c9288c9a02969f971cc60dd4
Michele Faucci Giannelli <faucci@chalmers.se>
2024-07-15 11:35:43 +0200

    Refactored measurement in methods and added loop on currents

--- tergite_autocalibration/lib/nodes/coupler/cz_characterisation/measurement.py
+++ tergite_autocalibration/lib/nodes/coupler/cz_characterisation/measurement.py
@@ -27,6 +27,7 @@ class CZ_Frequency_And_Amplitude(BaseMeasurement):
 
     def schedule_function(
         self,
+        cz_parking_currents: dict[str, np.ndarray],
         cz_pulse_frequencies: dict[str, np.ndarray],
         cz_pulse_amplitudes: dict[str, np.ndarray],
         cz_pulse_duration: float = 240e-9,
@@ -74,66 +75,83 @@ class CZ_Frequency_And_Amplitude(BaseMeasurement):
             An experiment schedule.
         """
         schedule = Schedule("CZ_Frequency_And_Amplitude", repetitions)
-        coupler = list(self.couplers.keys())[0]
-        qubits = coupler.split(sep="_")
 
+
+        cz_parking_currents_values = list(cz_pulse_amplitudes.values())[0]
         cz_frequency_values = np.array(list(cz_pulse_frequencies.values())[0])
         cz_amplitude_values = list(cz_pulse_amplitudes.values())[0]
 
-        all_couplers = [coupler]
-        for this_coupler in all_couplers:
-            if this_coupler in ["q21_q22", "q22_q23", "q23_q24", "q24_q25"]:
-                downconvert = 0
-            else:
-                downconvert = 4.4e9
-            schedule.add_resource(
-                ClockResource(
-                    name=coupler + ".cz", freq=-cz_frequency_values[0] + downconvert
-                )
-            )
+        for cz_current in cz_parking_currents_values:
+            self.redis_field = cz_current
+            schedule = self.loop_frequencies_and_aplitudes(schedule, cz_amplitude_values, cz_frequency_values, cz_pulse_duration)
 
-        number_of_amplitudess = len(cz_amplitude_values)
-
-        # The outer loop, iterates over all cz_frequencies
-        for freq_index, cz_frequency in enumerate(cz_frequency_values):
-            cz_clock = f"{coupler}.cz"
-            cz_pulse_port = f"{coupler}:fl"
-            schedule.add(
-                SetClockFrequency(
-                    clock=cz_clock, clock_freq_new=-cz_frequency + downconvert
-                ),
-            )
-
-            # The inner for loop iterates over cz pulse amplitude
-            for acq_index, cz_amplitude in enumerate(cz_amplitude_values):
-                this_index = freq_index * number_of_amplitudess + acq_index
-
-                relaxation = schedule.add(Reset(*qubits))
-
-                for this_qubit in qubits:
-                    schedule.add(X(this_qubit), ref_op=relaxation, ref_pt="end")
-
-                buffer = schedule.add(IdlePulse(4e-9))
-
-                schedule.add(ResetClockPhase(clock=coupler + ".cz"))
-                cz = schedule.add(
-                    SoftSquarePulse(
-                        duration=cz_pulse_duration,
-                        amp=cz_amplitude,
-                        port=cz_pulse_port,
-                        clock=cz_clock,
-                    ),
-                )
-                buffer = schedule.add(IdlePulse(4e-9))
-
-                for this_qubit in qubits:
-                    schedule.add(
-                        Measure(
-                            this_qubit, acq_index=this_index, bin_mode=BinMode.AVERAGE
-                        ),
-                        ref_op=buffer,
-                        rel_time=4e-9,
-                        ref_pt="end",
-                    )
         return schedule
+
+def loop_frequencies_and_aplitudes(self, schedule, cz_amplitude_values, cz_frequency_values, cz_pulse_duration) -> Schedule:
+    coupler = list(self.couplers.keys())[0]
+    all_couplers = [coupler]
+        
+    for this_coupler in all_couplers:
+        if this_coupler in ["q21_q22", "q22_q23", "q23_q24", "q24_q25"]:
+            downconvert = 0
+        else:
+            downconvert = 4.4e9
+        schedule.add_resource(
+            ClockResource(
+                name=coupler + ".cz", freq=-cz_frequency_values[0] + downconvert
+            )
+        )
+        
+
+    # The outer loop, iterates over all cz_frequencies
+    for freq_index, cz_frequency in enumerate(cz_frequency_values):
+        cz_clock = f"{coupler}.cz"
+        schedule.add(
+            SetClockFrequency(
+                clock=cz_clock, clock_freq_new=-cz_frequency + downconvert
+            ),
+        )
+        schedule = loop_amplitudes(coupler, schedule, cz_amplitude_values, freq_index, cz_pulse_duration)
+
+    return schedule
+
+def loop_amplitudes(self, schedule, coupler, cz_amplitude_values, freq_index, cz_pulse_duration) -> Schedule:
+    # The inner for loop iterates over cz pulse amplitude
+    number_of_amplitudess = len(cz_amplitude_values)
+    qubits = coupler.split(sep="_")
+    for acq_index, cz_amplitude in enumerate(cz_amplitude_values):
+        this_index = freq_index * number_of_amplitudess + acq_index
+
+        relaxation =  schedule.add(Reset(*qubits))
+
+        for this_qubit in qubits:
+             schedule.add(X(this_qubit), ref_op=relaxation, ref_pt="end")
+
+        buffer =  schedule.add(IdlePulse(4e-9))
+
+        schedule.add(ResetClockPhase(clock=coupler + ".cz"))
+        cz_clock = f"{coupler}.cz"
+        cz_pulse_port = f"{coupler}:fl"
+
+        cz = schedule.add(
+            SoftSquarePulse(
+                duration=cz_pulse_duration,
+                amp=cz_amplitude,
+                port=cz_pulse_port,
+                clock=cz_clock,
+            ),
+        )
+        buffer = schedule.add(IdlePulse(4e-9))
+
+        for this_qubit in qubits:
+             schedule.add(
+                Measure(
+                    this_qubit, acq_index=this_index, bin_mode=BinMode.AVERAGE
+                ),
+                ref_op=buffer,
+                rel_time=4e-9,
+                ref_pt="end",
+            )
+       
+    return schedule
 
