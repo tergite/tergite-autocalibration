79bcfd4698a81872c60f8e5fff309be8e4efa160
Eleftherios <elemos@chalmers.se>
2024-10-29 13:51:02 +0100

    fix: fixed removed credits

--- tergite_autocalibration/scripts/calibration_supervisor.py
+++ tergite_autocalibration/scripts/calibration_supervisor.py
@@ -3,6 +3,7 @@
 # (C) Copyright Eleftherios Moschandreou 2023, 2024
 # (C) Copyright Liangyu Chen 2023, 2024
 # (C) Copyright Stefan Hill 2024
+# (C) Copyright Michele Faucci Giannelli 2024
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
