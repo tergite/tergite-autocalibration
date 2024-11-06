23fb64e868a84a9428d5a63b13cbbb757b238788
Michele Faucci Giannelli <faucci@chalmers.se>
2024-11-06 12:47:51 +0100

    missing one place with the us name

--- tergite_autocalibration/lib/utils/node_factory.py
+++ tergite_autocalibration/lib/utils/node_factory.py
@@ -57,7 +57,7 @@ class NodeFactory:
             "reset_chevron": "Reset_Chevron_Node",
             "cz_characterisation_chevron": "CZ_Characterisation_Chevron_Node",
             "reset_calibration_ssro": "Reset_Calibration_SSRO_Node",
-            "cz_parametrisation_fix_duration": "CZParametrisationFixDurationNode",
+            "cz_parametrisation_fix_duration": "CZParametrizationFixDurationNode",
             "process_tomography_ssro": "Process_Tomography_Node",
             "cz_chevron": "CZ_Chevron_Node",
             "cz_optimize_chevron": "CZ_Optimize_Chevron_Node",
