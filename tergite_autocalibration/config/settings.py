e5f869eed5834638bc3de5a5c2aa5b3ca93d74eb
Stefan Hill <stefanhi@chalmers.se>
2024-11-14 17:09:27 +0100

    refactoring: move environment loading function to the helpers

--- tergite_autocalibration/config/settings.py
+++ tergite_autocalibration/config/settings.py
@@ -17,63 +17,20 @@
 import logging
 import os
 import typing
-import warnings
 from ipaddress import ip_address
 from pathlib import Path
 
 import redis
 from dotenv import dotenv_values
 
+from tergite_autocalibration.config.helpers import from_environment
+
 T = typing.TypeVar("T")
 config = dotenv_values(Path(__file__).parent.parent.parent.joinpath(".env"))
 
-
-def _from_config(key_name_: str, cast_: type = str, default: T = None) -> T:
-    """
-    Helper function to read keys from the .env file
-
-    Args:
-        key_name_: Name of the variable to read from .env
-        cast_: Cast variable to type T
-        default: Default value for the variable (will be checked for type T)
-
-    Returns:
-        Type-checked-and-casted variable from .env
-
-    """
-
-    if os.environ.get(key_name_) is not None:
-        try:
-            if cast_ is bool:
-                return eval(os.environ.get(key_name_))
-            return cast_(os.environ.get(key_name_))
-        except ValueError:
-            raise ValueError(
-                f"Variable with name {key_name_} from system environmental variables with value "
-                f"{os.environ.get(key_name_)} cannot be casted to type {cast_}"
-            )
-    elif key_name_ in config:
-        try:
-            if cast_ is bool:
-                return eval(config[key_name_])
-            return cast_(config[key_name_])
-        except ValueError:
-            raise ValueError(
-                f"Variable with name {key_name_} from .env with value {config[key_name_]} "
-                f"cannot be casted to type {cast_}"
-            )
-    elif default is not None:
-        # This is mainly a check for ourselves
-        assert isinstance(default, cast_)
-        return default
-    else:
-        warnings.warn(f"Cannot read {key_name_} from environment variables.")
-        return None
-
-
 # ---
 # Default prefix for paths
-DEFAULT_PREFIX = _from_config(
+DEFAULT_PREFIX = from_environment(
     "DEFAULT_PREFIX",
     cast_=str,
     default="calibration",
@@ -83,12 +40,12 @@ DEFAULT_PREFIX = _from_config(
 # Section with directory configurations
 
 # Root directory of the project
-ROOT_DIR = _from_config(
+ROOT_DIR = from_environment(
     "ROOT_DIR", cast_=Path, default=Path(__file__).parent.parent.parent
 )
 
 # Data directory to store plots and datasets
-DATA_DIR = _from_config("DATA_DIR", cast_=Path, default=ROOT_DIR.joinpath("out"))
+DATA_DIR = from_environment("DATA_DIR", cast_=Path, default=ROOT_DIR.joinpath("out"))
 
 # If the data directory does not exist, it will be created automatically
 if not os.path.exists(DATA_DIR):
@@ -96,42 +53,54 @@ if not os.path.exists(DATA_DIR):
     logging.info(f"Initialised DATA_DIR -> {DATA_DIR}")
 
 # Configuration directory to store additional configuration files
-CONFIG_DIR = _from_config(
+CONFIG_DIR = from_environment(
     "CONFIG_DIR", cast_=Path, default=ROOT_DIR.joinpath("configs")
 )
 
 # ---
 # Section with configuration files
-RUN_CONFIG = CONFIG_DIR.joinpath(_from_config("RUN_CONFIG", cast_=Path))
+RUN_CONFIG = CONFIG_DIR.joinpath(
+    from_environment("RUN_CONFIG", cast_=Path, default="run_config.toml")
+)
 
-CLUSTER_CONFIG = CONFIG_DIR.joinpath(_from_config("CLUSTER_CONFIG", cast_=Path))
-SPI_CONFIG = CONFIG_DIR.joinpath(_from_config("SPI_CONFIG", cast_=Path))
-DEVICE_CONFIG = CONFIG_DIR.joinpath(_from_config("DEVICE_CONFIG", cast_=Path))
+CLUSTER_CONFIG = CONFIG_DIR.joinpath(
+    from_environment("CLUSTER_CONFIG", cast_=Path, default="cluster_config.json")
+)
+SPI_CONFIG = CONFIG_DIR.joinpath(
+    from_environment("SPI_CONFIG", cast_=Path, default="spi_config.toml")
+)
+DEVICE_CONFIG = CONFIG_DIR.joinpath(
+    from_environment("DEVICE_CONFIG", cast_=Path, default="device_config.toml")
+)
 
-NODE_CONFIG = CONFIG_DIR.joinpath(_from_config("NODE_CONFIG", cast_=Path))
-USER_SAMPLESPACE = CONFIG_DIR.joinpath((_from_config("USER_SAMPLESPACE", cast_=Path)))
+NODE_CONFIG = CONFIG_DIR.joinpath(
+    from_environment("NODE_CONFIG", cast_=Path, default="node_config.toml")
+)
+USER_SAMPLESPACE = CONFIG_DIR.joinpath(
+    (from_environment("USER_SAMPLESPACE", cast_=Path, default="user_samplespace.py"))
+)
 
 BACKEND_CONFIG = Path(__file__).parent / "backend_config_default.toml"
 
 # ---
 # Section with other configuration variables
-CLUSTER_IP = ip_address(_from_config("CLUSTER_IP", cast_=str))
-SPI_SERIAL_PORT = _from_config("SPI_SERIAL_PORT", cast_=str)
+CLUSTER_IP = ip_address(from_environment("CLUSTER_IP", cast_=str))
+SPI_SERIAL_PORT = from_environment("SPI_SERIAL_PORT", cast_=str)
 
 # ---
 # Section for redis connectivity
-REDIS_PORT = _from_config("REDIS_PORT", cast_=int, default=6379)
+REDIS_PORT = from_environment("REDIS_PORT", cast_=int, default=6379)
 REDIS_CONNECTION = redis.Redis(decode_responses=True, port=REDIS_PORT)
 
 # ---
 # Section for plotting
-PLOTTING = _from_config("PLOTTING", cast_=bool, default=False)
+PLOTTING = from_environment("PLOTTING", cast_=bool, default=False)
 # This will be set in matplotlib
 PLOTTING_BACKEND = "tkagg" if PLOTTING else "agg"
 
 
 # ---
 # Section with connectivity definitions
-MSS_MACHINE_ROOT_URL = _from_config(
+MSS_MACHINE_ROOT_URL = from_environment(
     "MSS_MACHINE_ROOT_URL", cast_=str, default="http://localhost:8002"
 )
