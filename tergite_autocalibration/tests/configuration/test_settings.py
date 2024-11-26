23a14c655a89d4a09fc2a9c39bd1feefc97c924c
Stefan Hill <stefanhi@chalmers.se>
2024-11-26 15:24:56 +0100

    fix: add pipeline stage as in michele/dev/expand_pipeline_run_analysis

--- tergite_autocalibration/tests/configuration/test_settings.py
+++ tergite_autocalibration/tests/configuration/test_settings.py
@@ -10,4 +10,44 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-# TODO: Here we should test the .env configuration handler
+from tergite_autocalibration.config.env import EnvironmentConfiguration
+from tergite_autocalibration.config.globals import ENV, CONFIG
+from tergite_autocalibration.config.handler import ConfigurationHandler
+from tergite_autocalibration.config.package import ConfigurationPackage
+from tergite_autocalibration.tests.utils.fixtures import get_fixture_path
+
+
+def test_global_env_configuration():
+
+    env_configuration = EnvironmentConfiguration.from_dot_env(
+        get_fixture_path("configs", "env", "default.env")
+    )
+
+    assert ENV.default_prefix == env_configuration.default_prefix
+
+    assert ENV.root_dir == env_configuration.root_dir
+    assert ENV.data_dir == env_configuration.data_dir
+    assert ENV.config_dir == env_configuration.config_dir
+
+    assert ENV.backend_config == env_configuration.backend_config
+
+    assert ENV.cluster_ip == env_configuration.cluster_ip
+    assert ENV.spi_serial_port == env_configuration.spi_serial_port
+
+    assert ENV.redis_port == env_configuration.redis_port
+    assert ENV.plotting == env_configuration.plotting
+
+    assert ENV.mss_machine_root_url == env_configuration.mss_machine_root_url
+
+
+def test_global_configuration():
+    configuration_handler = ConfigurationHandler.from_configuration_package(
+        ConfigurationPackage.from_toml(
+            get_fixture_path(
+                "templates", "default_device_under_test", "configuration.meta.toml"
+            )
+        )
+    )
+
+    assert configuration_handler.run.filepath == CONFIG.run.filepath
+    assert configuration_handler.samplespace.filepath == CONFIG.samplespace.filepath
