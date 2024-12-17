67774f81290b2ad949c53293c4ff914197e68d1e
Eleftherios <elemos@chalmers.se>
2024-12-17 15:14:18 +0100

    fix: import

--- tergite_autocalibration/lib/nodes/characterization/randomized_benchmarking/measurement.py
+++ tergite_autocalibration/lib/nodes/characterization/randomized_benchmarking/measurement.py
@@ -25,8 +25,8 @@ from quantify_scheduler.schedules.schedule import Schedule
 
 import tergite_autocalibration.utils.clifford_elements_decomposition as cliffords
 from tergite_autocalibration.lib.base.measurement import BaseMeasurement
-from tergite_autocalibration.utils.extended_gates import Measure_RO_3state_Opt
-from tergite_autocalibration.utils.extended_transmon_element import ExtendedTransmon
+from tergite_autocalibration.utils.dto.extended_gates import Measure_RO_3state_Opt
+from tergite_autocalibration.utils.dto.extended_transmon_element import ExtendedTransmon
 
 
 class Randomized_Benchmarking_SSRO(BaseMeasurement):
