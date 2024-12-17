4cde256dfb8a55abe57a399501ad7775f818e99d
Eleftherios <elemos@chalmers.se>
2024-12-17 15:06:59 +0100

    fix: import

--- tergite_autocalibration/lib/nodes/readout/ro_amplitude_optimization/analysis.py
+++ tergite_autocalibration/lib/nodes/readout/ro_amplitude_optimization/analysis.py
@@ -18,7 +18,7 @@ from numpy.linalg import inv
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 
-from tergite_autocalibration.config.settings import REDIS_CONNECTION
+from tergite_autocalibration.config.globals import REDIS_CONNECTION
 from tergite_autocalibration.lib.base.analysis import (
     BaseAllQubitsAnalysis,
     BaseQubitAnalysis,
