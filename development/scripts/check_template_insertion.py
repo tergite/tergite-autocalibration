46d795c05fc101a614df189693eb5409bae7871f
Stefan Hill <stefanhi@chalmers.se>
2025-01-28 12:02:21 +0100

    wip: script draft with configuration j2 templates

--- development/scripts/check_template_insertion.py
+++ development/scripts/check_template_insertion.py
@@ -1,14 +1,39 @@
-from tergite_autocalibration.config.package import ConfigurationPackage
+import os
 
-if __name__ == '__main__':
+from tergite_autocalibration.config.globals import ENV
+from tergite_autocalibration.config.package import ConfigurationPackage
+from tergite_autocalibration.tools.cli.config import load
+
+if __name__ == "__main__":
 
     # Some function to get the number of qubits or the qubits themselves
 
-    # Load the configuration package
-    configuration_package = ConfigurationPackage()
+    # Load the default configuration package
+    load(template=".default")
 
-    # Move the configuration package to a temporary location
+    # Create a configuration package object for easier handling
+    configuration_package = ConfigurationPackage.from_toml(
+        os.path.join(ENV.config_dir, "configuration.meta.toml")
+    )
 
-    # Insert the template values
+    # Insert the template values for device configuration
+    configs_to_update = ["device_config", "run_config"]
 
-    # Rename the files inside the
+    # Iterate over the configurations to update
+    # Note: This can be looped at the moment, since there is a very simple logic behind updating
+    #       the configurations. It can also be solved in a more advanced way and more specific for
+    #       each single configuration file, but right now the only necessary parameter is the list
+    #       of qubits.
+    for config_name in configs_to_update:
+
+        # Get the path to the configuration template
+        config_template_path = os.path.join(
+            configuration_package.misc_filepaths["j2_templates"],
+            f"{config_name}.j2",
+        )
+
+        # Insert the template values for run configuration
+
+
+        # Write the configuration values to the .toml files
+        config_output_file_path = configuration_package.config_files[config_name]
