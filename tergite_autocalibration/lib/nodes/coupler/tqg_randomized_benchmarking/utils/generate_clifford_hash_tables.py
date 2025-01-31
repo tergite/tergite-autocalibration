f438054cbd019cb6f812609752356cabe228a768
Stefan Hill <stefanhi@chalmers.se>
2025-01-31 15:59:19 +0100

    fix: change print statement to logger

--- tergite_autocalibration/lib/nodes/coupler/tqg_randomized_benchmarking/utils/generate_clifford_hash_tables.py
+++ tergite_autocalibration/lib/nodes/coupler/tqg_randomized_benchmarking/utils/generate_clifford_hash_tables.py
@@ -22,14 +22,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+from os import mkdir
+from os.path import join, dirname, abspath
+from zlib import crc32
+
+import numpy as np
+
 from tergite_autocalibration.lib.nodes.coupler.tqg_randomized_benchmarking.utils.two_qubit_clifford_group import (
     SingleQubitClifford,
     TwoQubitClifford,
 )
-from os.path import join, dirname, abspath
-from os import mkdir
-import numpy as np
-from zlib import crc32
+from tergite_autocalibration.utils.logging import logger
 
 output_dir = join(abspath(dirname(__file__)), "clifford_hash_tables")
 try:
@@ -50,7 +53,7 @@ def construct_clifford_lookuptable(generator, indices):
 
 
 def generate_hash_tables():
-    print("Generating Clifford hash tables.")
+    logger.info("Generating Clifford hash tables.")
     single_qubit_hash_lut = construct_clifford_lookuptable(
         SingleQubitClifford, np.arange(24)
     )
@@ -64,7 +67,7 @@ def generate_hash_tables():
     with open(join(output_dir, "two_qubit_hash_lut.txt"), "w") as f:
         for h in two_qubit_hash_lut:
             f.write(str(h) + "\n")
-    print("Successfully generated Clifford hash tables.")
+    logger.info("Successfully generated Clifford hash tables.")
 
 
 if __name__ == "__main__":
