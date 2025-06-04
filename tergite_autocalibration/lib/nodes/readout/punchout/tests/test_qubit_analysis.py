bf255e7286e29d257f1c80a83e19649aaeb82a40
Michele Faucci Giannelli <faucci@chalmers.se>
2025-06-04 15:58:20 +0200

    fix bug

--- tergite_autocalibration/lib/nodes/readout/punchout/tests/test_qubit_analysis.py
+++ tergite_autocalibration/lib/nodes/readout/punchout/tests/test_qubit_analysis.py
@@ -10,15 +10,9 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-import os
 from pathlib import Path
-
-import matplotlib
-from matplotlib import pyplot as plt
 import pytest
 import xarray as xr
-from numpy import ndarray
-
 from tergite_autocalibration.lib.base.analysis import (
     BaseAnalysis,
     BaseQubitAnalysis,
@@ -26,8 +20,6 @@ from tergite_autocalibration.lib.base.analysis import (
 from tergite_autocalibration.lib.nodes.readout.punchout.analysis import (
     PunchoutQubitAnalysis,
 )
-
-from tergite_autocalibration.tests.utils.decorators import with_os_env
 
 
 def test_CanCreate():
@@ -44,45 +36,41 @@ def setup_data():
     )
     ds = xr.open_dataset(dataset_path)
 
-def amplitude_for_qubit(ds, qubit)    
+
+def amplitude_for_qubit(ds, qubit):
     ds = xr.merge(ds[var] for var in ["yq06"])
     ds.attrs["qubit"] = qubit
 
     a = PunchoutQubitAnalysis("name", ["redis_fields"])
-    qoi = a.setup_coupler_and_analyze(ds, qubit)
+    qoi = a.process_qubit(ds, qubit)
     return qoi["measure:pulse_amp"]
 
-def test_amplitude_for_q06(
-    setup_data: xr.Dataset,
-):
+
+def test_amplitude_for_q06(setup_data: xr.Dataset):
     ds = setup_data
     amplitude = amplitude_for_qubit(ds, "q06")
     assert amplitude["q06"] == 0.016
 
-def test_amplitude_for_q07(
-    setup_data: xr.Dataset,
-):
+
+def test_amplitude_for_q07(setup_data: xr.Dataset):
     ds = setup_data
     amplitude = amplitude_for_qubit(ds, "q07")
     assert amplitude["q06"] == 0.016
 
-def test_amplitude_for_q10(
-    setup_data: xr.Dataset,
-):
+
+def test_amplitude_for_q10(setup_data: xr.Dataset):
     ds = setup_data
     amplitude = amplitude_for_qubit(ds, "q10")
     assert amplitude["q06"] == 0.045
 
-def test_amplitude_for_q12(
-    setup_data: xr.Dataset,
-):
+
+def test_amplitude_for_q12(setup_data: xr.Dataset):
     ds = setup_data
     amplitude = amplitude_for_qubit(ds, "q12")
     assert amplitude["q06"] == 0.030
 
-def test_amplitude_for_q15(
-    setup_data: xr.Dataset,
-):
+
+def test_amplitude_for_q15(setup_data: xr.Dataset):
     ds = setup_data
     amplitude = amplitude_for_qubit(ds, "q15")
     assert amplitude["q06"] == 0.060
