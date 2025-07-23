540fc456abe682f1bd154f7162e0e00d7e18b463
eleftherios-m <elemos@chalmers.se>
2025-07-23 11:36:17 +0200

    chore: add headers

--- tergite_autocalibration/tools/plotly_browser/dash_browser.py
+++ tergite_autocalibration/tools/plotly_browser/dash_browser.py
@@ -1,3 +1,16 @@
+# This code is part of Tergite
+#
+# (C) Copyright Eleftherios Moschandreou 2025
+# (C) Chalmers Next Labs 2025
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
