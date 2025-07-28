abc2c09cf913d74ae03d29522a92766d47c2b3ac
Eleftherios <elemos@chalmers.se>
2025-07-28 12:53:28 +0200

    tests: its not worth it to test the callbacks

--- tergite_autocalibration/tests/test_browser.py
+++ tergite_autocalibration/tests/test_browser.py
@@ -16,7 +16,7 @@ import pytest
 
 
 # from tergite_autocalibration.tests.utils.reflections import iter_module_files
-# from tergite_autocalibration.utils.logging import logger
+from tergite_autocalibration.utils.logging import logger
 
 # Find the path to the root directory of the package
 # _library_folder = (
@@ -43,54 +43,3 @@ def test_folder_scan():
     measurements = folders[date][good_chain]
 
     assert len(measurements) == 13
-
-
-@with_os_env({"DATA_DIR": _DATA_DIR})
-def test_image_display(dash_duo):
-    outer = TEST_DATADIR / "2025-07-23"
-    inter = outer / "12-47-07_ro_frequency_two_state_optimization-ACTIVE"
-    inner = inter / "20250723-122627-108-80c1f0-resonator_spectroscopy"
-
-    dash_duo.start_server(app)
-
-    index = ""  # adjust if using "A"/"B" for Compare mode
-
-    # Get all dropdown components (they have class name 'Select-control' in Dash)
-    dropdowns = dash_duo.find_elements("div.Select-control")
-
-    print("*" * 100)
-    print("\nFound dropdowns:")
-    for i, dropdown in enumerate(dropdowns):
-        print(f"Dropdown {i}: {dropdown.text}")
-
-    assert len(dropdowns) == 0
-
-    # Select outer folder
-    # dash_duo.select_dcc_dropdown(
-    #     f'{{"type":"outer-selector","index":"{index}"}}', outer
-    # )
-
-    # dash_duo.wait_for_text_to_equal(
-    #     f'{{"type":"intermediate-selector","index":"{index}"}} label', inter, timeout=3
-    # )
-    #
-    # dash_duo.select_dcc_dropdown(
-    #     f'{{"type":"intermediate-selector","index":"{index}"}}', inter
-    # )
-    #
-    # dash_duo.wait_for_text_to_equal(
-    #     f'{{"type":"inner-selector","index":"{index}"}} label', inner, timeout=3
-    # )
-    #
-    # dash_duo.select_dcc_dropdown(
-    #     f'{{"type":"inner-selector","index":"{index}"}}', inner
-    # )
-    #
-    # # Wait for image to load in image tab
-    # dash_duo.wait_for_element(
-    #     f'{{"type":"tab-content","index":"{index}"}} img', timeout=5
-    # )
-    #
-    # # Optional: Assert something about the image or its src
-    # image = dash_duo.find_element(f'{{"type":"tab-content","index":"{index}"}} img')
-    # assert "data:image/png;base64" in image.get_attribute("src")
