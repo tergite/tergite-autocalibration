65df8f382fa3371ccd98b46bae34cc2eff734851
Eleftherios Moschandreou <elemos@chalmers.se>
2026-02-12 11:30:51 +0100

    tests: test that current is set from redis in coupler nodes

--- tergite_autocalibration/lib/base/tests/test_coupler_functionality.py
+++ tergite_autocalibration/lib/base/tests/test_coupler_functionality.py
@@ -14,28 +14,35 @@
 
 from tergite_autocalibration.config.globals import CONFIG
 from tergite_autocalibration.lib.nodes.coupler.cz_chevron.node import CZChevronNode
-from tergite_autocalibration.scripts.calibration_supervisor import (
-    CalibrationConfig,
-    HardwareManager,
-)
 from tergite_autocalibration.tests.utils.decorators import with_redis
 from tergite_autocalibration.tests.utils.fixtures import get_fixture_path
-from tergite_autocalibration.utils.dto.enums import MeasurementMode
 from tergite_autocalibration.utils.dto.extended_transmon_element import ExtendedTransmon
 
 redis_mock = get_fixture_path("redis", "standard_redis_mock.json")
+
+
+class DummySpiManager:
+    def __init__(self):
+        self._currents_dict = None  # internal storage
+
+    def set_dac_current(self, currents_dict: dict):
+        self._currents_dict = currents_dict
+
+    def get_dac_current(self) -> dict | None:
+        return self._currents_dict
 
 
 @with_redis(redis_mock)
 def test_set_parking_current_from_redis():
     ExtendedTransmon.close_all()  # ensure no other transmon objects are instantiated
     node = CZChevronNode(couplers=CONFIG.run.couplers, qubits=CONFIG.run.qubits)
-    cfg = CalibrationConfig(cluster_mode=MeasurementMode.dummy, cluster_ip=None)
+    # cfg = CalibrationConfig(cluster_mode=MeasurementMode.dummy, cluster_ip=None)
     # calib_sup = CalibrationSupervisor(config=cfg)
-    hwm = HardwareManager(cfg)
-    spi_manager = hwm.create_spi(CONFIG.run.couplers)
-    node.spi_manager = spi_manager
-    # assert isinstance(calib_sup.hardware_manager, HardwareManager)
-    # hardware_manager = HardwareManager(config=CONFIG)
+    # hwm = HardwareManager(cfg)
+    # spi_manager = hwm.create_spi(CONFIG.run.couplers)
+    node.spi_manager = DummySpiManager()
+
     node.set_parking_current_from_redis()
-    breakpoint()
+    currents_dict = node.spi_manager.get_dac_current()
+    assert "q00_q01" in currents_dict
+    assert currents_dict["q00_q01"] == "0.00095"
