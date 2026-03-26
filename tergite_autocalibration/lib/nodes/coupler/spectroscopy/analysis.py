d4cbcc80e40848998812eaea87f07cc678070dfa
Eleftherios Moschandreou <elemos@chalmers.se>
2026-03-26 13:26:36 +0100

    feature: WIP couples spectroscopy , qubit peaks

--- tergite_autocalibration/lib/nodes/coupler/spectroscopy/analysis.py
+++ tergite_autocalibration/lib/nodes/coupler/spectroscopy/analysis.py
@@ -17,6 +17,7 @@ from typing import List
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray
+from scipy.signal import find_peaks
 
 from tergite_autocalibration.config.globals import REDIS_CONNECTION
 from tergite_autocalibration.lib.base.analysis import (
@@ -112,71 +113,135 @@ class CouplerSpectroscopyAnalysis(BaseCouplerAnalysis):
         return crossing_currents
 
     def detect_peaks(self, qubit_specs_dataarray: xarray.DataArray):
+        for coord in qubit_specs_dataarray.coords:
+            coord = str(coord)
+            if "frequencies" in coord:
+                frequency_coord = coord
+                break
         detected_frequencies = []
         detected_currents = []
         for current in self.dc_currents:
-            ds = qubit_specs_dataarray.sel({self.current_coord: current})
-            freq_analysis = QubitSpectroscopyMaxThresholdQubitAnalysis(ds)
-            qubit_frequency = freq_analysis.process_qubit()
+            array = qubit_specs_dataarray.sel({self.current_coord: current})
+            # freq_analysis = QubitSpectroscopyMaxThresholdQubitAnalysis(array)
+            # qubit_frequency = freq_analysis.process_qubit()
+            noise_level = array.std().item()
+            peak, props = find_peaks(array, prominence=3 * noise_level)
+            if peak.size == 0:
+                continue
+            qubit_frequency = array[frequency_coord].values[peak].item()
             detected_frequencies.append(qubit_frequency)
             detected_currents.append(current)
         return detected_frequencies, detected_currents
 
     def analyze_coupler(self):
-        for coord in self.dataset[self.data_var].coords:
-            if "currents" in coord:
-                self.current_coord = coord
-                self.dc_currents = self.dataset[self.current_coord].values
+        for coord_name in self.dataset.coords:
+            coord_name = str(coord_name)
+            coord = self.dataset[coord_name]
+            if "currents" in coord_name:
+                self.current_coord = coord_name
+                self.dc_currents = coord.values
+        for coord_name in self.control_qubit_data_var.coords:
+            coord_name = str(coord_name)
+            coord = self.dataset[coord_name]
+            if "qubit_frequencies" in coord_name:
+                self.control_qubit_frequencies_coord = coord_name
+            elif "resonator_frequencies" in coord_name:
+                self.control_resonator_frequencies_coord = coord_name
+        for coord_name in self.target_qubit_data_var.coords:
+            coord_name = str(coord_name)
+            coord = self.dataset[coord_name]
+            if "qubit_frequencies" in coord_name:
+                self.target_qubit_frequencies_coord = coord_name
+            elif "resonator_frequencies" in coord_name:
+                self.target_resonator_frequencies_coord = coord_name
+        for dim_name in self.control_qubit_data_var.dims:
+            dim_name = str(dim_name)
+            if "common_dimension" in dim_name:
+                self.control_common_dim = dim_name
+        for dim_name in self.target_qubit_data_var.dims:
+            dim_name = str(dim_name)
+            if "common_dimension" in dim_name:
+                self.target_common_dim = dim_name
 
-        breakpoint()
         threshold = 2000000
         spurious_threshold = 300000
         min_interval = 0.0003
         resonator_crossings_interval = 0.00015
 
-        control_qubit_magnitudes = xarray.ufuncs.abs(self.control_qubit_data_var)
-        target_qubit_magnitudes = xarray.ufuncs.abs(self.target_qubit_data_var)
+        control_magnitudes = xarray.ufuncs.abs(self.control_qubit_data_var)
+        target_magnitudes = xarray.ufuncs.abs(self.target_qubit_data_var)
+        control_qubit_spectroscopy = control_magnitudes.where(
+            control_magnitudes[self.control_qubit_frequencies_coord].notnull(),
+            drop=True,
+        )
+        control_resonator_spectroscopy = control_magnitudes.where(
+            control_magnitudes[self.control_resonator_frequencies_coord].notnull(),
+            drop=True,
+        )
+        target_qubit_spectroscopy = target_magnitudes.where(
+            target_magnitudes[self.target_qubit_frequencies_coord].notnull(),
+            drop=True,
+        )
+        target_resonator_spectroscopy = target_magnitudes.where(
+            target_magnitudes[self.target_resonator_frequencies_coord].notnull(),
+            drop=True,
+        )
+
+        # clean up dimensions
+        control_qubit_spectroscopy = control_qubit_spectroscopy.swap_dims(
+            {self.control_common_dim: self.control_qubit_frequencies_coord}
+        ).drop_vars(self.control_resonator_frequencies_coord)
+        control_resonator_spectroscopy = control_resonator_spectroscopy.swap_dims(
+            {self.control_common_dim: self.control_resonator_frequencies_coord}
+        ).drop_vars(self.control_qubit_frequencies_coord)
+        target_qubit_spectroscopy = target_qubit_spectroscopy.swap_dims(
+            {self.target_common_dim: self.target_qubit_frequencies_coord}
+        ).drop_vars(self.target_resonator_frequencies_coord)
+        target_resonator_spectroscopy = target_resonator_spectroscopy.swap_dims(
+            {self.target_common_dim: self.target_resonator_frequencies_coord}
+        ).drop_vars(self.target_qubit_frequencies_coord)
+
         (
             self.control_qubit_detected_frequencies,
             self.control_qubit_detected_currents,
-        ) = self.detect_peaks(control_qubit_magnitudes)
+        ) = self.detect_peaks(control_qubit_spectroscopy)
         self.target_qubit_detected_frequencies, self.target_qubit_detected_currents = (
-            self.detect_peaks(target_qubit_magnitudes)
+            self.detect_peaks(target_qubit_spectroscopy)
         )
-        self.resonator_crossing_points = []
-        self.control_crossing_currents = self._find_crossing_currents(
-            self.control_qubit_detected_currents,
-            self.control_qubit_detected_frequencies,
-            threshold,
-            spurious_threshold,
-            min_interval,
-            self.resonator_crossing_points,
-            resonator_crossings_interval,
-        )
-        self.target_crossing_currents = self._find_crossing_currents(
-            self.target_qubit_detected_currents,
-            self.target_qubit_detected_frequencies,
-            threshold,
-            spurious_threshold,
-            min_interval,
-            self.resonator_crossing_points,
-            resonator_crossings_interval,
-        )
-
-        analysis_succesful = True
-        analysis_result = {
-            "control_qubit_crossing_points": {
-                "value": self.control_crossing_currents,
-                "error": 0,
-            },
-            "target_qubit_crossing_points": {
-                "value": self.target_crossing_currents,
-                "error": 0,
-            },
-        }
-
-        qoi = QOI(analysis_result, analysis_succesful)
-        return qoi
+        # self.resonator_crossing_points = []
+        # self.control_crossing_currents = self._find_crossing_currents(
+        #     self.control_qubit_detected_currents,
+        #     self.control_qubit_detected_frequencies,
+        #     threshold,
+        #     spurious_threshold,
+        #     min_interval,
+        #     self.resonator_crossing_points,
+        #     resonator_crossings_interval,
+        # )
+        # self.target_crossing_currents = self._find_crossing_currents(
+        #     self.target_qubit_detected_currents,
+        #     self.target_qubit_detected_frequencies,
+        #     threshold,
+        #     spurious_threshold,
+        #     min_interval,
+        #     self.resonator_crossing_points,
+        #     resonator_crossings_interval,
+        # )
+        #
+        # analysis_succesful = True
+        # analysis_result = {
+        #     "control_qubit_crossing_points": {
+        #         "value": self.control_crossing_currents,
+        #         "error": 0,
+        #     },
+        #     "target_qubit_crossing_points": {
+        #         "value": self.target_crossing_currents,
+        #         "error": 0,
+        #     },
+        # }
+        #
+        # qoi = QOI(analysis_result, analysis_succesful)
+        # return qoi
 
     def plotter(self, figures_dictionary: dict[str, list]):
         """
@@ -187,41 +252,41 @@ class CouplerSpectroscopyAnalysis(BaseCouplerAnalysis):
              containing the anticrossing figure for that coupler
         """
         figures_list = []
-        fig, (ax1, ax2) = plt.subplots(2, 1)
-        control_qubit_magnitudes = xarray.ufuncs.abs(self.control_qubit_data_var)
-        target_qubit_magnitudes = xarray.ufuncs.abs(self.target_qubit_data_var)
-        control_qubit_magnitudes.plot(ax=ax1, x=self.current_coord)
-        target_qubit_magnitudes.plot(ax=ax2, x=self.current_coord)
-
-        ax1.scatter(
-            self.control_qubit_detected_currents,
-            self.control_qubit_detected_frequencies,
-            s=52,
-            c="red",
-        )
-        for cross_current in self.control_crossing_currents:
-            ax1.axvline(
-                cross_current,
-                color="grey",
-                linestyle="dashed",
-                linewidth=2,
-            )
-        ax2.scatter(
-            self.target_qubit_detected_currents,
-            self.target_qubit_detected_frequencies,
-            s=52,
-            c="red",
-        )
-        for cross_current in self.target_crossing_currents:
-            ax2.axvline(
-                cross_current,
-                color="grey",
-                linestyle="dashed",
-                linewidth=2,
-            )
-        figures_list.append(fig)
-        figures_dictionary[self.coupler] = figures_list
-        return
+        # fig, (ax1, ax2) = plt.subplots(2, 1)
+        # control_qubit_magnitudes = xarray.ufuncs.abs(self.control_qubit_data_var)
+        # target_qubit_magnitudes = xarray.ufuncs.abs(self.target_qubit_data_var)
+        # control_qubit_magnitudes.plot(ax=ax1, x=self.current_coord)
+        # target_qubit_magnitudes.plot(ax=ax2, x=self.current_coord)
+        #
+        # ax1.scatter(
+        #     self.control_qubit_detected_currents,
+        #     self.control_qubit_detected_frequencies,
+        #     s=52,
+        #     c="red",
+        # )
+        # for cross_current in self.control_crossing_currents:
+        #     ax1.axvline(
+        #         cross_current,
+        #         color="grey",
+        #         linestyle="dashed",
+        #         linewidth=2,
+        #     )
+        # ax2.scatter(
+        #     self.target_qubit_detected_currents,
+        #     self.target_qubit_detected_frequencies,
+        #     s=52,
+        #     c="red",
+        # )
+        # for cross_current in self.target_crossing_currents:
+        #     ax2.axvline(
+        #         cross_current,
+        #         color="grey",
+        #         linestyle="dashed",
+        #         linewidth=2,
+        #     )
+        # figures_list.append(fig)
+        # figures_dictionary[self.coupler] = figures_list
+        # return
 
 
 class CouplerSpectroscopyNodeAnalysis(BaseAllCouplersAnalysis):
